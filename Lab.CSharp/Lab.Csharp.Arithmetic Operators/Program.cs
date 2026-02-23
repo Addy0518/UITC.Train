e891cbfa40dcec027eedc852d45b8610db36287c
Andy Chen <angey920518@gmail.com>
2026-02-23 17:19:32 +0800

    feat:arithmetic operators完成

---
+++ Lab.CSharp/Lab.Csharp.Arithmetic Operators/Program.cs
@@ -0,0 +1,18 @@
+﻿int friends = 5;
+
+//friends = friends + 1;
+//friends += 2;
+//friends++;
+
+//friends -= 2;
+//friends--;
+
+//friends = friends / 2;//除數
+//friends /= 2;
+
+//friends = friends * 3;//乘數
+//friends *= 3;
+
+int remindfriends = friends % 2;//餘數
+
+Console.WriteLine(remindfriends);
