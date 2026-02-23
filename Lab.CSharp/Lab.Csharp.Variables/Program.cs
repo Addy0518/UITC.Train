c2ebfc043f281fdba0da6f5138137f87379343cf
Andy Chen <angey920518@gmail.com>
2026-02-23 16:39:21 +0800

    feat:variables 完成

---
+++ Lab.CSharp/Lab.Csharp.Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿int x;//宣告變數
+x = 123;//初始化變數
+
+int y = 456;//合再一起
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+int z=x+y;
+
+Console.WriteLine(z);// 可以直接列出計算結果
+
+string name = "Andy Chen";//字串
+int year = 21;//整數
+double height = 300.1;//浮點數
+char symbol = '@';//字元
+bool alive=true;//布林值
+
+string username=symbol+name;//字串相加
+
+Console.WriteLine($"My age is {year}");//可以顯示字串+變數
+Console.WriteLine($"My height is {height}cm");
+Console.WriteLine($"Are You alive?,{alive}");
+Console.WriteLine($"My symbol is {symbol}");
+Console.WriteLine($"My name is {name}");
+Console.WriteLine($"username is {username}");
