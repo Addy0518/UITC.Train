abdf7d6a6b0ddc2303bd0736b231fa809e7b90d5
Andy Chen <angey920518@gmail.com>
2026-02-23 16:21:30 +0800

    feat: output完成

---
+++ Lab.CSharp/Lab.Csharp.Output/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Hey!");
+Console.WriteLine("Hello, World!");
+
+//這是註解
+/*
+ * 
+ * 
+ * 這也是註解
+ *
+ *
+ *
+ *
+ */
+
+Console.WriteLine("Andy\bChen");
+/*
+ * \t是表格
+ * \n是換行
+ * \b是退一格
+ */
+Console.ReadKey();//讓程式暫停,在按一次enter結束
