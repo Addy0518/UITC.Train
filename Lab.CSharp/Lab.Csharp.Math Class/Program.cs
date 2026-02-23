120dcf4ea5d7be816e95ec457c18a49bd7cd4dff
Andy Chen <angey920518@gmail.com>
2026-02-23 17:28:47 +0800

    feat:math class完成

---
+++ Lab.CSharp/Lab.Csharp.Math Class/Program.cs
@@ -0,0 +1,12 @@
+﻿double x = -3.94;
+double y = 8;
+
+double a=Math.Pow(x,2);//次方
+double b = Math.Sqrt(x);//平方根
+double c = Math.Abs(x);//絕對值
+double d = Math.Round(x);//四捨五入
+double e=Math.Ceiling(x);//無條件進位
+double f = Math.Floor(x);//無條件捨去
+double g = Math.Max(x, y);//取最大值
+double h = Math.Min(x,y);//取最小值
+Console.WriteLine(f);
