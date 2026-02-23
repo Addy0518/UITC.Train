0f2cd6aa3f9e5a8f041e4ce61a047ffdc96499e5
Andy Chen <angey920518@gmail.com>
2026-02-23 18:03:37 +0800

    feat:if statements完成

---
+++ Lab.CSharp/Lab.Csharp.if statement/Program.cs
@@ -0,0 +1,29 @@
+﻿//Console.WriteLine("輸入年齡 :　");
+//int age= Convert.ToInt32(Console.ReadLine());
+
+//if (age <= 18)
+//{
+//    Console.WriteLine("必須年滿18!");
+//}
+//else if (age < 0)
+//{
+//    Console.WriteLine("你還沒出生!");
+//}
+//else
+//{
+//    Console.WriteLine("ok!");
+//}
+
+
+Console.WriteLine("輸入姓名 :　");
+string name = Console.ReadLine();
+
+if (name != "" && name != null)
+{
+    Console.WriteLine($"Hello , {name}");
+
+}
+else
+{
+    Console.WriteLine("沒輸入阿喂!");
+}
