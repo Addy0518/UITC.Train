6ba4eaf3fd856a452752bfd1d8485c0ef4006aff
Andy Chen <angey920518@gmail.com>
2026-02-23 17:44:15 +0800

    feat:hypotenuse calculator

---
+++ Lab.CSharp/Lab.Csharp.hypotenuse calculator program/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("輸入A邊 :");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("輸入B邊 :");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double c = Math.Sqrt((a * a) + (b * b));
+
+Console.WriteLine($"斜邊是 : {c}");
