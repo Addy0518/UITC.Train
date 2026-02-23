0f67ebf382e4d06aaa04ce901d56c03ae63ab7c8
Andy Chen <angey920518@gmail.com>
2026-02-23 17:04:31 +0800

    feat:User Input完成

---
+++ Lab.CSharp/Lab.Csharp.User Input/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What Your Name?");
+
+string username=Console.ReadLine();//從鍵盤輸入資料
+
+
+Console.WriteLine("What Youe age?");
+int age=Convert.ToInt32(Console.ReadLine());//轉型成int
+
+Console.WriteLine($"Hello,{username}");
+Console.WriteLine($"Your age is {age}");
