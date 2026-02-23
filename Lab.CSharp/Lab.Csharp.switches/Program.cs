47bffd483e6d729d7e6ceb3723fb4e8539bdd158
Andy Chen <angey920518@gmail.com>
2026-02-23 18:09:58 +0800

    feat:switches

---
+++ Lab.CSharp/Lab.Csharp.switches/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+Console.WriteLine("What day is today?");
+string day = Console.ReadLine();
+
+switch (day)//switch case語法
+{
+   case "Monday":
+        Console.WriteLine("星期一"); break;
+
+   case"Tuesday":
+        Console.WriteLine("星期二"); break;
+
+   default: Console.WriteLine("要輸入星期幾!");break;
+}
