8347a96a1cc0244569bd44bbfcfe57b91b2d8154
Andy Chen <angey920518@gmail.com>
2026-02-24 09:10:49 +0800

    feat:for loops完成

---
+++ Lab.CSharp/Lab.Csharp.for loops/Program.cs
@@ -0,0 +1,15 @@
+﻿//for (int i = 0; i < 10; i++)//for迴圈,可以設定迴圈變數,以及條件跟增遞的方式
+//{
+//    Console.WriteLine(i);
+
+//}
+
+for (int i = 10; i > 0; i--)//遞減
+{
+    Console.WriteLine(i);
+   
+
+}
+Console.WriteLine("Happy New Year!");
+
+Console.ReadKey();
