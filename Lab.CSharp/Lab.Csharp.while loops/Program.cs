19c545f48e2170b79dad34a6f8038e7d037bbf76
Andy Chen <angey920518@gmail.com>
2026-02-24 08:39:26 +0800

    feat:while loops完成

---
+++ Lab.CSharp/Lab.Csharp.while loops/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("輸入姓名 :　");
+string name = Console.ReadLine();
+
+//while迴圈,沒達到條件就一直執行
+while (String.IsNullOrWhiteSpace(name))
+{
+    Console.Write("輸入姓名 :　");
+    name = Console.ReadLine();
+}
+
+
+Console.WriteLine($"Hello , {name}");
