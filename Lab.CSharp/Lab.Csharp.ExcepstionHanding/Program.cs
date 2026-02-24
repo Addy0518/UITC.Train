138e94bb37a4741112c7ffe43d51a836fd14305e
Andy Chen <angey920518@gmail.com>
2026-02-24 14:26:56 +0800

    feat:ExcepstionHanding完成

---
+++ Lab.CSharp/Lab.Csharp.ExcepstionHanding/Program.cs
@@ -0,0 +1,31 @@
+﻿int x;
+int y;
+
+try // try..catch..finally,用來捕獲錯誤並防止程式中斷,finally是不管有沒有錯誤最後都會執行
+{
+
+    Console.Write("請輸入第一個數字 : ");
+    x = Convert.ToInt32(Console.ReadLine());
+
+
+    Console.Write("請輸入第二個數字 : ");
+    y = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(x / y);
+}
+catch (FormatException ex) // 格式錯誤
+{
+    Console.WriteLine("請輸入數字!");
+}
+catch (DivideByZeroException exx) // 整數除以0的錯誤
+{
+    Console.WriteLine("不能除以0!");
+}
+catch (Exception exxx) // 最終防線,用來抓所有錯誤的
+{
+    Console.WriteLine($"有錯誤!,{exxx.Message}");
+}
+finally
+{
+    Console.WriteLine("end");
+}
