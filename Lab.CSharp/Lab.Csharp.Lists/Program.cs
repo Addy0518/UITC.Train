29638002682b6a4b01c44dd3bf8bb552f3ef4765
Andy Chen <angey920518@gmail.com>
2026-02-24 18:00:22 +0800

    feat:List完成

---
+++ Lab.CSharp/Lab.Csharp.Lists/Program.cs
@@ -0,0 +1,11 @@
+﻿List<String> Food = new List<String>();
+
+Food.Add("pizza");
+Food.Add("banana");
+Food.Add("rice");
+
+Food.Remove("pizza");// 去除
+Food.Insert(0, "soup");// 插入指定位置
+Console.WriteLine(Food.Count());// 數量
+Console.WriteLine(Food.Contains("rice")); // 包含
+Food.Sort();// 排列
