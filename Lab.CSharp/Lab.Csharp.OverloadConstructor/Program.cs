95ad189ff17c8d6fae8e91efeece3ecaf102e0aa
Andy Chen <angey920518@gmail.com>
2026-02-24 15:53:51 +0800

    feat:OverloadConstructor完成

---
+++ Lab.CSharp/Lab.Csharp.OverloadConstructor/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+// 可以建立多個相同名稱的建構函式
+
+Pizza piz = new Pizza("good cheese", "spicy");
+Pizza piz2 = new Pizza("good cheese");
+
+
+
+
+// 先建立類別
+class Pizza
+{
+    public string cheese;
+    public string taste;
+
+    // 在類別裡做建構函式
+    public Pizza(string cheese, string taste)// 初始化的時候帶入參數
+    { 
+       this.cheese = cheese;
+       this.taste = taste;
+    
+    }
+
+    public Pizza(string cheese)
+    { 
+      this.cheese=cheese;
+    
+    }
+
+}
