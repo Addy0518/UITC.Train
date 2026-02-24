20bb86277787fd8b7dcc5f4a8db19a99932a7442
Andy Chen <angey920518@gmail.com>
2026-02-24 18:06:29 +0800

    feat:ListOfObject完成

---
+++ Lab.CSharp/Lab.Csharp.ListsOfObject/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+List<Player> players = new List<Player>();
+
+Player player1 = new Player("Andy");
+Player player2 = new Player("Eddy");
+
+players.Add(player1);
+players.Add(player2);
+
+foreach (Player p in players)
+{
+    Console.WriteLine(p);
+}
+
+class Player
+{
+    public string username;
+
+    public Player(string username)
+    { 
+       this.username = username;
+    }
+    // 把字串方法覆蓋
+    public override string ToString()
+    {
+        return username;
+    }
+}
