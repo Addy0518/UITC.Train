313e50811637c2c8649d77bf83b4726500e493f2
Andy Chen <angey920518@gmail.com>
2026-02-24 16:33:43 +0800

    feat:ArrayOfObject完成

---
+++ Lab.CSharp/Lab.Csharp.ArrayofObject/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+// 陣列實體化物件
+
+Car[] carArray = { new Car("Tesla"), new Car("Honda") };
+
+foreach (Car car in carArray)
+{
+    Console.WriteLine(car.model);
+
+}
+
+
+class Car
+{
+    public string model;
+
+    public Car(string model)
+    { 
+      this.model = model;
+    }
+
+
+}
