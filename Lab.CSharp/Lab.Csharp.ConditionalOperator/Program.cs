783f3ccfeef21df98912f23d7f3fa5876e3eaf7c
Andy Chen <angey920518@gmail.com>
2026-02-24 14:40:04 +0800

    feat:ConditionalOperator完成

---
+++ Lab.CSharp/Lab.Csharp.ConditionalOperator/Program.cs
@@ -0,0 +1,14 @@
+﻿double temperature = 21;
+
+// 這是if..else的寫法
+if (temperature >= 25)
+{
+    Console.WriteLine("外面很熱!");
+}
+else
+{
+    Console.WriteLine("外面很冷!");
+}
+
+// 這是另一種寫法=>三元運算子, 設定條件?有達成:沒達成
+Console.WriteLine((temperature >= 25) ? "外面很熱!" : "外面很冷!");
