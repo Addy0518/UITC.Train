d04d15473c698028fc1a6ee4f19d3b2b2fd6f458
Andy Chen <angey920518@gmail.com>
2026-02-24 13:43:45 +0800

    feat:Array完成

---
+++ Lab.CSharp/Lab.Csharp.Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿String[] cars = { "BMW", "BENZ", "HONDA" };//第一種宣告陣列的方式
+
+String[] fruit = new String[3];//第2種
+
+fruit[0] = "apple";
+fruit[1] = "banana";
+fruit[2] = "watermelon";
+
+
+
+cars[0] = "TESLA";//更換陣列元素
+
+//Console.WriteLine(cars[0]);
+
+//用迴圈顯示陣列內所有元素
+for (int i = 0; i < cars.Length; i++)
+{
+    Console.WriteLine(cars[i]);
+}
+
+for (int i = 0; i < fruit.Length; i++)
+{
+    Console.WriteLine(fruit[i]);
+}
