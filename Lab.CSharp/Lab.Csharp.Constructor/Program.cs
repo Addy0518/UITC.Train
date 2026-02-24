39934823e5d0a2764aa9270746e8e1299ab13e90
Andy Chen <angey920518@gmail.com>
2026-02-24 15:35:02 +0800

    feat:Constructor完成

---
+++ Lab.CSharp/Lab.Csharp.Constructor/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+using System.Drawing;
+
+Car c1 = new Car("黑色",350000);
+
+c1.YourCar();
+
+
+
+class Car
+{
+    string color;
+    decimal price;
+
+
+    public Car(string color,decimal price)
+    { 
+       this.color = color;
+       this.price = price;
+    }
+
+    public void YourCar()
+    {
+        Console.WriteLine($"Your Car Is {color},And The Price Is {price}$");
+    }
+
+}
