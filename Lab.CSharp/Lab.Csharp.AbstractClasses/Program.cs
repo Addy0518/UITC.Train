85facdae6010f5769dc7c8cab8dcc0f8a8b5bca9
Andy Chen <angey920518@gmail.com>
2026-02-24 16:26:06 +0800

    feat:AbstractClasses完成

---
+++ Lab.CSharp/Lab.Csharp.AbstractClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+
+// 抽象類別=>無法實例化,增加安全性
+// 可以被繼承的子類別覆蓋
+//Speed sp = new Speed();
+
+Car car1=new Car();
+car1.Go();
+public class Car : Speed
+{
+    public override void Go()
+    {
+        Console.WriteLine("GOGO");
+    }
+}
+
+public abstract class Speed
+{
+    public int speed = 20;
+
+    public abstract void Go();
+   
+
+}
