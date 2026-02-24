86ff8e354b3bd4ba634bfa5351b83a9c8d74250d
Andy Chen <angey920518@gmail.com>
2026-02-24 17:24:09 +0800

    feat:Interfaces完成

---
+++ Lab.CSharp/Lab.Csharp.Interfaces/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+// 繼承=>可以定義方法給其他類別繼承,也比較安全
+
+Car car=new Car();
+car.Run();
+Boat boat=new Boat();
+boat.Run();
+boat.Water();
+
+
+
+interface IRun
+{ 
+   void Run();
+}
+interface IWater
+{
+    void Water();
+}
+
+
+class Car:IRun
+{
+    public void Run()
+    {
+        Console.WriteLine("Drive a Car");
+    }
+}
+class Boat:IRun,IWater
+{
+    public void Run()
+    {
+        Console.WriteLine("Drive a Boat");
+    }
+
+    public void Water()
+    {
+        Console.WriteLine("Water");
+    }
+
+}
