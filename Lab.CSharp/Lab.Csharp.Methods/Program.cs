0c3eeebbca658fccf68d4e58ada878e542f2614e
Andy Chen <angey920518@gmail.com>
2026-02-24 13:54:28 +0800

    feat:Methods完成

---
+++ Lab.CSharp/Lab.Csharp.Methods/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string name = "Andy";
+int age = 23;
+
+singHappyBirthday(name,age);
+
+// static靜態方法 不用實體化呼叫
+// 可以把一些重複會用到的邏輯寫進方法,重複使用
+static void singHappyBirthday(string name,int age)// 可以讓方法有接口能接變數(注意:要使用相同型態的變數)
+{
+    Console.WriteLine("祝你生日快樂!");
+    Console.WriteLine("祝你生日快樂!");
+    Console.WriteLine($"{name},{age}歲快樂!");
+}
