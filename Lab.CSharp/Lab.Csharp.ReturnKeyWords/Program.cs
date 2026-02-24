394e3e75ec11c7537410fd136cc3651149bd1404
Andy Chen <angey920518@gmail.com>
2026-02-24 14:00:21 +0800

    feat:ReturnKeyWords完成

---
+++ Lab.CSharp/Lab.Csharp.ReturnKeyWords/Program.cs
@@ -0,0 +1,21 @@
+﻿
+double a;
+double b;
+double result;
+
+Console.Write("輸入數字a : ");
+a=Convert.ToDouble(Console.ReadLine());
+
+Console.Write("輸入數字b : ");
+b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write($"計算結果 : {Muiltpy(a,b)}");
+
+
+
+static double Muiltpy(double x,double y)//方法可以回傳值,設定回傳值的型態並return
+{
+
+    return x * y;
+ 
+}
