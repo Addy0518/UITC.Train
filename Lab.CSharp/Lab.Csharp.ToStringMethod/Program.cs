8f3cf5ae072953330d6d245d46a3e1602ec66eef
Andy Chen <angey920518@gmail.com>
2026-02-24 17:04:24 +0800

    feat:ToStringMethod完成

---
+++ Lab.CSharp/Lab.Csharp.ToStringMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Car car1 = new Car("Tesla", "Black");
+
+
+// 本來的型別string就被我自訂的ToString()覆蓋了
+Console.WriteLine(car1);
+
+
+class Car
+{
+    public string model;
+    public string color;
+
+    public Car(string model, string color)
+    {
+        this.model = model;
+        this.color = color;
+    }
+
+    public override string ToString()
+    {
+        return $"This is A Car";
+    }
+}
