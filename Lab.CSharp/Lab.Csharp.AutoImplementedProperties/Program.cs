f4768634e99fc16fc3f6c4b5f3aafcf7f071ea3e
Andy Chen <angey920518@gmail.com>
2026-02-25 09:54:33 +0800

    feat:AutoImplementProperties完成

---
+++ Lab.CSharp/Lab.Csharp.AutoImplementedProperties/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+Car car = new Car("Tesla");
+
+Console.WriteLine(car.Model);
+
+
+class Car
+{
+    // 如果沒有特別要設定條件的話,就可以簡寫成像這樣,這就是自動屬性
+    public string Model { get; set; }
+
+    
+    public Car(string model)
+    { 
+       this.Model = model;
+    }
+
+}
