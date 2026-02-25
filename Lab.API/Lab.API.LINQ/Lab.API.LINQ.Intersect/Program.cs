2389b28e2bda701f30ea4813e9de762fcf36e7e9
Andy Chen <angey920518@gmail.com>
2026-02-25 16:16:10 +0800

    feat:Intersect完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.Intersect/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.Intersect/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+IEnumerable<int> coll = [1, 2, 3];
+IEnumerable<int> coll2 = [2, 3, 4];
+
+
+
+coll.Intersect(coll2).Dump();
