d9ea22905c2a4054b17942d3235e3baee81f2824
Andy Chen <angey920518@gmail.com>
2026-02-25 13:48:33 +0800

    feat:Select完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.Select/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.Select/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+
+IEnumerable<int> collection = [1, 2, 3, 4, 5];
+
+// Select的Landa函式裡,分別可以指定索引跟元素
+collection.Select((x,i) => $"{i},{x}").Dump();
