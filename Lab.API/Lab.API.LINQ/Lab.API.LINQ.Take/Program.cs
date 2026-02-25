be101b6ac69274169f19c537a8ab7dbf8bbef6d7
Andy Chen <angey920518@gmail.com>
2026-02-25 13:37:04 +0800

    feat:Take完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.Take/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.Take/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+
+IEnumerable<object> collection = [1, "2", 3, 4, 5];
+
+//Take:從0開始選幾個
+collection.Take(2).Dump();
