72e1b8e5d057ddafd1729705e33e648e9dcb3805
Andy Chen <angey920518@gmail.com>
2026-02-25 16:24:03 +0800

    feat:UnionBy & IntercertBy & ExceptBy完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.UnionBy & IntersectBy & Except/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.UnionBy & IntersectBy & Except/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Dumpify;
+
+List<Person> coll = [new(1, "A", 15), new(2, "B", 16)];
+List<Person> coll2 = [new(1, "A", 15)];
+
+coll.UnionBy(coll2, x => x.Id).Dump();
+coll.IntersectBy(coll2.Select(x=>x.Id), x => x.Id).Dump();
+coll.ExceptBy(coll2.Select(x => x.Id), x => x.Id).Dump();
+
+record Person(int Id, string Name, int age);
+
+
+
+
