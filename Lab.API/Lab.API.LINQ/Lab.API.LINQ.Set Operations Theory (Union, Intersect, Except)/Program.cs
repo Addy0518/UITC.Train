8dfdb440a6cbaf34b3ce575fa9327b468325d329
Andy Chen <angey920518@gmail.com>
2026-02-25 16:18:22 +0800

    feat:Set Operations Theory (Union, Intersect, Except)完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.Set Operations Theory (Union, Intersect, Except)/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.Set Operations Theory (Union, Intersect, Except)/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Dumpify;
+IEnumerable<int> coll = [1, 2, 3];
+IEnumerable<int> coll2 = [2, 3, 4];
+
+
+coll.Union(coll2).Dump();
+coll.Intersect(coll2).Dump();
+coll.Except(coll2).Dump();
+
+
+
+
