e6d18ebb4fc13cccb6c1cc68b4cb4c2f3b9cc587
Andy Chen <angey920518@gmail.com>
2026-02-25 16:15:58 +0800

    feat:GroupJoin完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.GroupJoin/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.GroupJoin/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Dumpify;
+
+IEnumerable<Person> coll = [new(0, "aa", 18), new(1, "bb", 19)];
+IEnumerable<Product> coll1 = [new(0, "ccc"), new(0, "aaaac"), new(1, "ddd")];
+
+coll.GroupJoin(
+    coll1,
+    person => person.Id,
+    product => product.ProductId,
+    (person, product) => $"{person.Name} bought {string.Join(',',product)}").Dump();
+
+
+
+
+record Person(int Id, string Name, int age);
+record Product(int ProductId, string Name);
