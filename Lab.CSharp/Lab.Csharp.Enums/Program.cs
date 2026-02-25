4d13e1cfb0929c1a551eb8f3e6363aaae67abeea
Andy Chen <angey920518@gmail.com>
2026-02-25 10:11:55 +0800

    feat:Enums完成

---
+++ Lab.CSharp/Lab.Csharp.Enums/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+//Console.WriteLine((int)Car.Benz+" "+Car.Honda);
+
+// 可以拿取它的名稱,或是值
+string name = CarPrice.Honda.ToString();
+int price = (int)CarPrice.Honda;
+double count = Count(CarPrice.Tesla);
+
+Console.WriteLine($"CarName={name} , CarPrice={price},Count={count}");
+
+
+static double Count(CarPrice car)
+{
+    double Price = (int)car * 0.75;
+    return Price;
+}
+
+
+// 列舉:可以自己設定一套命名標準,名稱會依預設排序(0=>1=>2...),也可以自訂他的值
+enum CarPrice
+{
+  Tesla=300,
+  Honda=500,
+  Benz=600
+
+
+}
