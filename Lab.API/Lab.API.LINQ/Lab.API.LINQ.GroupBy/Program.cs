b0028aea7a8f19eb716e2b8ecc84d2ccaee02c81
Andy Chen <angey920518@gmail.com>
2026-02-25 16:15:46 +0800

    feat:GroupBy完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.GroupBy/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.GroupBy/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Dumpify;
+
+IEnumerable<Person> coll = [new(0, "aa", 18), new(1, "bb", 19)];
+
+
+IGrouping<int,Person> group=coll.GroupBy(x => x.Id).Last().Dump();
+
+
+foreach (var person in group)
+{
+    person.Dump();
+  
+}
+
+
+
+record Person(int Id, string Name, int age);
