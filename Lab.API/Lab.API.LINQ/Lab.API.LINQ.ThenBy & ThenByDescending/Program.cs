09de5767c6cd0aaa75e35e96463e29b5e416739d
Andy Chen <angey920518@gmail.com>
2026-02-25 16:17:20 +0800

    feat:ThenBy & ThenByDecending完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.ThenBy & ThenByDescending/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.ThenBy & ThenByDescending/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+
+IEnumerable<Person> coll = [new(0, "aa", 18), new(1, "bb", 19)];
+
+coll.OrderBy(x=>x.Name).ThenBy(x=>x.age).Dump();
+
+record Person(int Id, string Name, int age);
