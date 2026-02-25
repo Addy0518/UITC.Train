b63c0eba7a0a55b9a29ad1b9d70a6d77ac941bec
Andy Chen <angey920518@gmail.com>
2026-02-25 16:22:18 +0800

    feat:ToLookUp完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.ToLookUp/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.ToLookUp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Dumpify;
+
+IEnumerable<Person> coll = [
+      new("A",15),
+      new("B",16),
+      new("C",17),
+
+    ];
+
+
+coll.ToLookup(x => x.age)[15].Single().Name.Dump();
+
+record Person(string Name, int age);
+    
+
+
+
