e42e859b4cae72fbb477c0268aaa15551955e7b2
Andy Chen <angey920518@gmail.com>
2026-02-25 13:58:41 +0800

    feat:Cast完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.Cast/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.Cast/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+
+IEnumerable<object> collection = [1, 2, 3, 4, 5];
+
+// 強行別轉換
+collection.Cast<int>().Dump();
