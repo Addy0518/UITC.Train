276cca866116c47425bea263e2207ee77194c74e
Andy Chen <angey920518@gmail.com>
2026-02-25 16:21:44 +0800

    feat:OrderDecending 完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.OrderDescending & OrderByDescending/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.OrderDescending & OrderByDescending/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+
+IEnumerable<int> coll = [3, 1, 2];
+
+coll.OrderDescending().Dump();
+coll.OrderByDescending(x => x * 2).Dump();
