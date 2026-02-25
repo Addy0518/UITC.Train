8bb5a4ba8babc67ce3df20b3d26d5b0880d22bc3
Andy Chen <angey920518@gmail.com>
2026-02-25 14:27:06 +0800

    feat:Append & Perpend完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.Append & Prepend/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.Append & Prepend/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+
+IEnumerable<int> collection = [1, 2, 3, 4, 5];
+
+//在開頭加上元素
+collection.Append(5).Dump();
+//在結尾加上元素
+collection.Prepend(5).Dump();
+
