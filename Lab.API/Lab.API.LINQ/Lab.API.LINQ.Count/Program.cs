511a38dc130bcb936193a5a30c50c2f58953f962
Andy Chen <angey920518@gmail.com>
2026-02-25 14:29:11 +0800

    feat:Count完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.Count/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.Count/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+
+IEnumerable<int> collection = [1, 2, 3, 4, 5];
+
+// 數量計算
+collection.Count().Dump();
