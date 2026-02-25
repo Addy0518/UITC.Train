b801602a8288f63104a2545b103f943557df047f
Andy Chen <angey920518@gmail.com>
2026-02-25 16:11:50 +0800

    feat:Aggregate2完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ. Aggregate (overload 2)/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ. Aggregate (overload 2)/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+IEnumerable<int> collection = [1, 2, 3, 4, 5];
+
+
+Console.WriteLine(collection.Aggregate(10,(x, y) =>x+y));
