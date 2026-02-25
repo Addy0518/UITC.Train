3c24c6d1986f3c9e2ecdcf2a6d938de11cdda745
Andy Chen <angey920518@gmail.com>
2026-02-25 13:39:11 +0800

    feat:SkipLast & TakeLast完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.SkipLast & TakeLast/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.SkipLast & TakeLast/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+
+IEnumerable<object> collection = [1, "2", 3, 4, 5];
+
+// 從末端作為起點
+collection.SkipLast(3).Dump();
+collection.TakeLast(3).Dump();
