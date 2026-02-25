b7469ad3421843b3bce6443d7e345026938fb228
Andy Chen <angey920518@gmail.com>
2026-02-25 09:50:27 +0800

    feat:GettersAndSetters完成

---
+++ Lab.CSharp/Lab.Csharp.GettersAndSetters/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+Car car1 = new Car();
+
+
+
+car1.Speed = 10000;
+
+Console.WriteLine(car1.Speed);
+
+
+class Car
+{
+
+    private int speed;
+
+    // 屬性：set設定進來的值,也可以在這裡給他條件,再用get return回去
+    public int Speed
+    {
+       get{ return speed; }
+       set
+       {
+            if (value > 500)
+            {
+                speed = 500;
+            }
+            else 
+            {
+             this.speed = value; 
+            }
+
+           
+        
+        }
+    }
+
+}
