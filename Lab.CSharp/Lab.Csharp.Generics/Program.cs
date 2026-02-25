2d15daac798b2c3bf5311969c22a6435154bc6fc
Andy Chen <angey920518@gmail.com>
2026-02-25 10:20:46 +0800

    feat:Generics完成

---
+++ Lab.CSharp/Lab.Csharp.Generics/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arr = { 1, 2, 3 };
+double[] arr2 = { 1.0, 2.0, 3.0 };
+string[] arr3 = { "A", "B", "C" };
+
+DisplayEments(arr);
+DisplayEments(arr2);
+DisplayEments(arr3);
+
+// 泛型:通用所有類別,可以$在要接收相同參數但是不同型態時
+static void DisplayEments<T>(T[] array)
+{
+    foreach (T item in array)
+    {
+        Console.Write(item+" ");
+    }
+    Console.WriteLine();
+}
