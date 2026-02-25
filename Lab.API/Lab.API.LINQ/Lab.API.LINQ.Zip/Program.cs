5e7cc99cdf11755d0ec701c6f994996b9c727f2f
Andy Chen <angey920518@gmail.com>
2026-02-25 16:24:13 +0800

    feat:Zip完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.Zip/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.Zip/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+
+IEnumerable<int> coll = [1, 2, 3];
+IEnumerable<string> coll1 = ["A", "b"];
+IEnumerable<string> coll2 = ["xx", "xxx"];
+
+coll.Zip(coll1, coll2).Dump();
