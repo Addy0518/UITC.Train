bce3cfe932c5ad34f093e3521e404f785082bc5d
Andy Chen <angey920518@gmail.com>
2026-02-25 13:53:50 +0800

    feat:SelectMany完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.SelectMany/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.SelectMany/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+
+IEnumerable<List<int>> collection = [[1,2,3],[4,5,6]];
+
+// SelectMany可以展開多個陣列或集合,然後再用select選擇裡面的元素
+collection.SelectMany(x => x.Select(x=>x.ToString())).Dump();
