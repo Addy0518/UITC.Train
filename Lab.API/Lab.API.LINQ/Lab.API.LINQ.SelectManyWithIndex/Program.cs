19a1ca509cf957cbd46def843910608ab7d677d7
Andy Chen <angey920518@gmail.com>
2026-02-25 13:56:38 +0800

    feat:SelectManyWithIndex完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.SelectManyWithIndex/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.SelectManyWithIndex/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dumpify;
+
+IEnumerable<List<int>> collection = [[1, 2, 3], [4, 5, 6]];
+
+// 用索引指定哪一個集合
+collection.SelectMany((x,i) => x.Select(x => $"{i},{x}".ToString())).Dump();
