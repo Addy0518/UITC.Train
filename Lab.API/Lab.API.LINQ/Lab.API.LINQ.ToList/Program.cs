7b15adb917cf96ce949172b2d8f7780d6ff620c5
Andy Chen <angey920518@gmail.com>
2026-02-25 16:16:33 +0800

    feat:ToList完成

--- Lab.API/Lab.API.LINQ/Lab.API.LINQ.ToList/Program.cs
+++ Lab.API/Lab.API.LINQ/Lab.API.LINQ.ToList/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Dumpify;
+
+IEnumerable<int> collection = [1, 2, 3];
+
+collection.ToList().Dump();
