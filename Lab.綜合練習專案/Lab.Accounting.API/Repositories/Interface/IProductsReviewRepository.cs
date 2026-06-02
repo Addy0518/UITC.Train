b20114986ff14e1138bef64fd75b54055113567b
Andy Chen <angey920518@gmail.com>
2026-06-02 17:55:59 +0800

    chore : repo 繼續 , 要記得做 ReviewStatus 的 enum

---
+++ Lab.綜合練習專案/Lab.Accounting.API/Repositories/Interface/IProductsReviewRepository.cs
@@ -0,0 +1,4 @@
+﻿namespace Lab.Accounting.API.Repositories.Interface
+{
+    public interface IProductsReviewRepository { }
+}
---
+++ Lab.綜合練習專案/Lab.Accounting.API/Repositories/ProductsReviewRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace Lab.Accounting.API.Repositories
+{
+    public class ProductsReviewRepository(DBConnecting connecting) : IProductsReviewRepository
+    {
+        /// <summary>
+        /// 新增審核
+        /// </summary>
+        /// <param name="request">賣家商品資訊</param>
+        /// <returns>影響列數</returns>
+        public async Task<int> CreateProductsReview(ProductsInsertRequest request)
+        {
+            using var conn = connecting.CreateConnecting();
+            var sql =
+                @"Insert Into [ProductsReview] (
+                  UserName, UserAccount, UserPassword, UserPhone,UserAddress,CreateTime,UpdateTime,IsDelete
+                ) 
+                values 
+                  (
+                    @UserName, @UserAccount, @UserPassword, 
+                    @UserPhone,@UserAddress,GetDate(),GetDate(),@IsDelete
+                  );
+                Select 
+                  Cast(
+                    Scope_Identity() as int
+                  );";
+
+            return await conn.QuerySingleAsync<UserResponse>(sql, userInformation);
+        }
+    }
+}
