44b3f66ab6fc02cdcc68452a38c447d7fcfeb082
Andy Chen <angey920518@gmail.com>
2026-06-08 17:49:28 +0800

    chore : 開始寫賣家數據 dashboardRepository

---
+++ Lab.綜合練習專案/Lab.Accounting.API/Repositories/DashboradRepository.cs
@@ -0,0 +1,38 @@
+﻿using Lab.Accounting.API.Common.Requests.Store;
+
+namespace Lab.Accounting.API.Repositories.Interface
+{
+    public class DashboradRepository(DBConnecting connecting) : IDashboradRepository
+    {
+        /// <summary>
+        /// 賣場註冊
+        /// </summary>
+        /// <param name="sellerId">賣家 ID</param>
+        /// <returns>影響列數</returns>
+        //public async Task<double> GetTotalRevenue()
+        //{
+        //    using var conn = connecting.CreateConnecting();
+
+        //    var sql =
+        //        @"INSERT INTO [dbo].[seller]
+        //                    (userid,
+        //                     sellername,
+        //                     sellerunifiednumber,
+        //                     sellercompanyname,
+        //                     createtime,
+        //                     updatetime,
+        //                     IsDelete)
+        //        VALUES      (@userid,
+        //                     @SellerName,
+        //                     @SellerUnifiedNumber,
+        //                     @SellerCompanyName,
+        //                     GetDate(),
+        //                     GetDate(),
+        //                     @IsDelete)
+
+        //        SELECT Cast(@@ROWCOUNT AS INT) ";
+
+        //    return await conn.ExecuteAsync(sql, seller);
+        //}
+    }
+}
---
+++ Lab.綜合練習專案/Lab.Accounting.API/Repositories/Interface/IDashboradRepository.cs
@@ -0,0 +1,6 @@
+﻿using Lab.Accounting.API.Common.Requests.Store;
+
+namespace Lab.Accounting.API.Repositories.Interface
+{
+    public interface IDashboradRepository { }
+}
